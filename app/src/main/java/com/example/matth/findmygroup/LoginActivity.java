91ff86c9c67cc85e78d639675247a1ea4deeede1
Matthew <matthew.d.zirbel@gmail.com>
2017-10-22 07:56:23 -0400

    Merge branch 'master' of https://github.com/mdzirbel/FindMyGroup
    # Conflicts:
    #	app/src/main/java/com/example/matth/findmygroup/MainActivity.java

--- app/src/main/java/com/example/matth/findmygroup/LoginActivity.java
+++ app/src/main/java/com/example/matth/findmygroup/LoginActivity.java
@@ -81,6 +81,8 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
         }
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 
     private void checkPermissions() {
