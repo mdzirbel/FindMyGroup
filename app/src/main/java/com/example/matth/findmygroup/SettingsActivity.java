b226e27229b9c6872eab0315285a3a0da18f7910
Vyyom-Kelkar <kelkar.20@osu.edu>
2017-10-22 05:09:13 -0400

    finished settings activity and layout

--- app/src/main/java/com/example/matth/findmygroup/SettingsActivity.java
+++ app/src/main/java/com/example/matth/findmygroup/SettingsActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 public class SettingsActivity extends AppCompatActivity {
 
     private static final String ACCOUNT_INFO = "accountInfo";
@@ -18,7 +17,11 @@ public class SettingsActivity extends AppCompatActivity {
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String acctInfo = prefs.getString(ACCOUNT_INFO, null);
-        String[] listItems = acctInfo.split("%");
+
+        String[] listItems = new String[0];
+        if (acctInfo != null && acctInfo.contains("%")) {
+            listItems = acctInfo.split("%");
+        }
 
         ArrayAdapter adapter = new ArrayAdapter<>(this,
                 R.layout.activity_settings_listview, listItems);
